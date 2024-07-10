64af3d3f075e019b6541b92968eddc1d6908662d
QosimovRahmonjonn <qosimovrahmonjon2023@gmail.com>
2024-07-10 09:40:35 +0500

    RTK api

--- api/api.ts
+++ api/api.ts
@@ -4,6 +4,13 @@ const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
     baseUrl: "https://dummyjson.com/",
+    prepareHeaders: (headers) => {
+      const token = localStorage.getItem("x-auth-token");
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
   }),
   tagTypes: ["Products", "Users"],
   endpoints: () => ({}),
